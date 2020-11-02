95d44c6e962f3979e931abcd3eaafb850771808f
谭璇 <tanxuan_2013@163com>
2020-11-02 22:51:27 +0800

    init

---
+++ website/src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: Tan Xuan
+ * @Date: 2020-10-30 22:12:30
+ * @LastEditors: Tan Xuan
+ * @LastEditTime: 2020-10-30 22:14:25
+ * @Description: File content
+-->
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ website/src/views/Home.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: Tan Xuan
+ * @Date: 2020-10-30 22:12:30
+ * @LastEditors: Tan Xuan
+ * @LastEditTime: 2020-10-30 22:18:06
+ * @Description: File content
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to vue" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
